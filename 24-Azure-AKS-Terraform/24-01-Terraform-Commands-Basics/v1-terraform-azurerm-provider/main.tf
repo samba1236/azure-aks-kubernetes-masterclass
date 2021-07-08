63316e92ea9aa78e5588844e746ef7c9f3d3e689
Samba Siva Reddy <samba.akepati@gmail.com>
2021-07-08 11:08:32 +0530

    Update main.tf

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   # Version is optional
   # Terraform recommends to pin to a specific version of provider
-  #version = "=2.35.0"
+  version = "=2.66.0"
   #version = "~>2.35.0"
   #version = "~> 2.37.0"
   features {}
